7b2bb6f11d2e8a2f1b8d85f384519feafc55bb0d
BrewingWeasel <weeaseled@gmail.com>
2023-08-04 16:51:55 -0400

    feat: allow aliases

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,12 @@ fn main() {
         shell_functions.add_env_var(&mut enter_file, k, v.as_str().unwrap());
     }
 
+    for (k, v) in doc["aliases"].as_hash().unwrap() {
+        let k = k.as_str().unwrap();
+        shell_functions.remove_alias(&mut out_file, k);
+        shell_functions.add_alias(&mut enter_file, k, v.as_str().unwrap());
+    }
+
     add_commands(&doc["on_enter"], &mut enter_file);
     add_commands(&doc["on_exit"], &mut out_file);
 
@@ -40,6 +46,8 @@ fn main() {
 trait Shell {
     fn add_env_var(&self, file: &mut String, k: &str, v: &str);
     fn remove_env_var(&self, file: &mut String, k: &str);
+    fn add_alias(&self, file: &mut String, k: &str, v: &str);
+    fn remove_alias(&self, file: &mut String, k: &str);
 }
 
 struct Fish {}
@@ -52,6 +60,12 @@ impl Shell for Fish {
     fn remove_env_var(&self, file: &mut String, k: &str) {
         file.push_str(&format!("set -e {}\n", k))
     }
+    fn add_alias(&self, file: &mut String, k: &str, v: &str) {
+        file.push_str(&format!("alias {k} \"{v}\"\n"))
+    }
+    fn remove_alias(&self, file: &mut String, k: &str) {
+        file.push_str(&format!("functions -e {k}\n"))
+    }
 }
 
 impl Shell for Bash {
@@ -60,6 +74,12 @@ impl Shell for Bash {
     }
     fn remove_env_var(&self, file: &mut String, k: &str) {
         file.push_str(&format!("unset {}\n", k))
+    }
+    fn add_alias(&self, file: &mut String, k: &str, v: &str) {
+        file.push_str(&format!("alias {k}=\"{v}\"\n"))
+    }
+    fn remove_alias(&self, file: &mut String, k: &str) {
+        file.push_str(&format!("unalias {k}\n"))
     }
 }
 
