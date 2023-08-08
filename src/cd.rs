6c183efb525b492e6872f12a32363f9e2369ba27
BrewingWeasel <weeaseled@gmail.com>
2023-08-08 15:00:28 -0400

    fix: leaving active directory from subdirectory not running exit script

--- src/cd.rs
+++ src/cd.rs
@@ -1,4 +1,7 @@
-use std::{env, path::PathBuf};
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
 
 use crate::{bash, fish, Shell};
 
@@ -21,17 +24,16 @@ pub fn envail_cd(dir: Option<String>, active_dirs: Option<Vec<String>>, shell: S
     let dir = PathBuf::from(dir).canonicalize().unwrap();
     let cur_dir = env::current_dir().expect("unable to get current dir!");
 
-    let child_of_active = is_child_of_active_dir(&dir, active_dirs);
+    let child_of_active = is_child_of_active_dir(&dir, &active_dirs);
 
     if !dir.starts_with(&cur_dir) && !child_of_active && cur_dir.join(".envail").exists() {
-        if !cur_dir.join(format!(".envail/build/{shell_name}")).exists() {
-            println!("envail build;")
-        }
-        println!(
-            "source {}/.envail/build/{shell_name}/leave;",
-            cur_dir.display()
-        );
-        println!("_envail_delete_from_active {};", cur_dir.display())
+        exit_dir(&cur_dir, shell_name)
+    }
+
+    // If a user is in a path further away from their active directory and they leave it, we still
+    // want to run the scripts
+    for old_active_dir in is_leaving_active_dir(&dir, active_dirs) {
+        exit_dir(&PathBuf::from(old_active_dir), shell_name)
     }
 
     shell_functions.run_cd(&dir);
@@ -45,7 +47,18 @@ pub fn envail_cd(dir: Option<String>, active_dirs: Option<Vec<String>>, shell: S
     }
 }
 
-fn is_child_of_active_dir(dir: &PathBuf, active_dirs: Option<Vec<String>>) -> bool {
+fn exit_dir(cur_dir: &Path, shell_name: &str) {
+    if !cur_dir.join(format!(".envail/build/{shell_name}")).exists() {
+        println!("envail build;")
+    }
+    println!(
+        "source {}/.envail/build/{shell_name}/leave;",
+        cur_dir.display()
+    );
+    println!("_envail_delete_from_active {};", cur_dir.display())
+}
+
+fn is_child_of_active_dir(dir: &PathBuf, active_dirs: &Option<Vec<String>>) -> bool {
     if let Some(dirs) = active_dirs {
         for i in dirs {
             if dir.starts_with(i) {
@@ -54,4 +67,16 @@ fn is_child_of_active_dir(dir: &PathBuf, active_dirs: Option<Vec<String>>) -> bo
         }
     }
     false
+}
+
+fn is_leaving_active_dir(dir: &PathBuf, active_dirs: Option<Vec<String>>) -> Vec<String> {
+    let mut dirs_left = vec![];
+    if let Some(dirs) = active_dirs {
+        for i in dirs {
+            if !dir.starts_with(&i) {
+                dirs_left.push(i)
+            }
+        }
+    }
+    dirs_left
 }
